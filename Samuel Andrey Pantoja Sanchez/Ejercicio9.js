a695c5950fd8588532697eed283967eed9a093e6
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:52:03 -0500

    Reenviado Ej8

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio9.js
@@ -0,0 +1,10 @@
+let equipoDev = ["Ana", "Juan"];
+let equipoQA = ["Carlos", "Maria"];
+
+// 1
+let equipoCompleto = equipoDev.concat(equipoQA);
+
+// Mostrar resultados
+console.log("Equipo Dev:", equipoDev);
+console.log("Equipo QA:", equipoQA);
+console.log("Equipo Completo:", equipoCompleto);
