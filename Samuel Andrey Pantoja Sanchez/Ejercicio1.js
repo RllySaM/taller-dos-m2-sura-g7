880a8af99c569a3f760a4ce4ac1b55b19761c4bc
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:47:50 -0500

    Reenviado Ej1

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio1.js
@@ -0,0 +1,8 @@
+let puntos = [10, 20, 30, 40];
+
+// 1
+let puntosDoble = puntos.map(punto => punto * 2);
+
+// 2
+console.log("Arreglo original:", puntos);
+console.log("Arreglo duplicado:", puntosDoble);
