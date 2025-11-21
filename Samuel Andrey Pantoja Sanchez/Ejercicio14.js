2ba53b8e2b66df8e8f1b83ebda30edb54a3af106
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:54:54 -0500

    Reenviado Ej14

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio14.js
@@ -0,0 +1,8 @@
+let receta = ["Paso 1", "Paso 2", "Paso 3"];
+
+// 1
+let recetaInversa = [...receta].reverse();
+
+// 2
+console.log("Receta original:", receta);
+console.log("Receta inversa:", recetaInversa);
