9b03a950a59c5de4682cabf38520e597b779d3e6
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:51:04 -0500

    Reenviado Ej7

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio7.js
@@ -0,0 +1,11 @@
+let original = ["A", "B", "C"];
+
+// 1
+let clon = original.slice();
+
+// 2
+clon.push("D");
+
+// Mostrar resultados
+console.log("Arreglo original:", original);
+console.log("Clon modificado:", clon);
