3e2fa57809280608e6ff59957d707a69bde61f0d
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:51:44 -0500

    Reenviado Ej8

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio8.js
@@ -0,0 +1,8 @@
+let competidores = ["Oro", "Plata", "Bronce", "Mención 1", "Mención 2"];
+
+// 1
+let podio = competidores.slice(0, 3);
+
+// Mostrar resultados
+console.log("Arreglo original de competidores:", competidores);
+console.log("Arreglo del podio:", podio);
