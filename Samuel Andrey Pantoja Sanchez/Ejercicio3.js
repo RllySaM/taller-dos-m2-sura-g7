9d219ce92473307392156977ee0b315167583829
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:49:01 -0500

    Reenviado Ej3

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio3.js
@@ -0,0 +1,8 @@
+let notas = [8, 5, 10, 3, 7];
+
+// 1
+let aprobadas = notas.filter(nota => nota >= 6);
+
+// Mostrar resultados
+console.log("Arreglo original de notas:", notas);
+console.log("Arreglo de notas aprobadas:", aprobadas);
