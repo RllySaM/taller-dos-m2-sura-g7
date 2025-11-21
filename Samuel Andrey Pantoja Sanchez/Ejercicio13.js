5402694d6111b26e6f61bcd2c1a3b7242ac7a442
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:54:27 -0500

    Reenviado Ej13

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio13.js
@@ -0,0 +1,8 @@
+let invitados = ["Zapata", "Alvarez", "Gomez", "Bernal"];
+
+// 1
+let invitadosOrdenados = [...invitados].sort();
+
+// 2
+console.log("Lista original de invitados:", invitados);
+console.log("Lista ordenada de invitados:", invitadosOrdenados);
